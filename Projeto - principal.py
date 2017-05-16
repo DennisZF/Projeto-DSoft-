895464203466adb18e9e0739e442911a0f168760
annabcm <annabcm@al.insper.edu.br>
2017-05-16 16:13:49 -0300

    Tirando excessos

--- Projeto - principal.py
+++ Projeto - principal.py
@@ -4,11 +4,7 @@ from tkinter import *
 import tkinter.font as tkFont
 import tkinter.ttk as ttk
 from Classe_login import Login
-from Classe_Cadastro import Cadastro
-from Classe_Menu_principal import Menu_principal
-from Classe_Sair import Sair
 from Classe_Database import Database
-from Classe_Calendario import Calendario
 
 D= Database()
 abre=Login(D)
