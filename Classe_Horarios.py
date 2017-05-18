95e0fca8b56c4dddedea8155b79b9989736c81f7
annabcm <annabcm@al.insper.edu.br>
2017-05-18 17:49:21 -0300

    tirando comentarios desnecessarios

--- Classe_Horarios.py
+++ Classe_Horarios.py
@@ -30,14 +30,3 @@ class Horarios:
 	def novadata(self,item,data, dados, menu):
 		self.tree.destroy()
 		self.selecionado(item,data, dados, menu)
-#		segundaframe = ttk.Frame()
-#		self.tree = ttk.Treeview(columns=["Horário", "Máquina", "Nome", "Quantidade dde pessoas"], show="headings")
-#		vsb = ttk.Scrollbar(orient="vertical",command=self.tree.yview)
-#		hsb = ttk.Scrollbar(orient="horizontal",command=self.tree.xview)
-#		self.tree.configure(yscrollcommand=vsb.set,xscrollcommand=hsb.set)
-#		self.tree.grid(column=0, row=0, sticky='nsew', in_=segundaframe)
-#		vsb.grid(column=1, row=0, sticky='ns', in_=segundaframe)
-#		hsb.grid(column=0, row=1, sticky='ew', in_=segundaframe)
-#		
-#		item.frame.grid_columnconfigure(0, weight=1)
-#		item.frame.grid_rowconfigure(0, weight=1)
