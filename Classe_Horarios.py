99f5566c92001461b0ad531cd9e6b069c5b0c5a3
annabcm <annabcm@al.insper.edu.br>
2017-05-18 23:09:20 -0300

    sem agendamento de quinta

--- Classe_Horarios.py
+++ Classe_Horarios.py
@@ -1,31 +1,35 @@
 from tkinter import *
 import tkinter.font as tkFont
 import tkinter.ttk as ttk
+import datetime
 
 class Horarios:
 	def selecionado(self,item,data, dados, menu):
-		#self.horarios = Listbox(item.frame)
-		#self.horarios.pack(side= RIGHT, padx=70)
-		#print(item.dados.horarios)
-		#for a in item.dados.horarios[data]:
-	#		self.horarios.insert(END,a)
 		self.dados = dados
-		self.tree = ttk.Treeview(columns=('Máquina', 'Nome', 'Quantidade de pessoas', 'Tempo'))
+		dia = int(data[0:2])
+		mes = int(data[3:5])
+		ano = int(data[6:])
+
+		self.tree = ttk.Treeview(columns=('Setor', 'Nome', 'Quantidade de pessoas', 'Tempo agendado'))
 		self.tree.heading('#0', text='Horário')
-		self.tree.heading('#1', text='Máquina')
+		self.tree.heading('#1', text='Setor')
 		self.tree.heading('#2', text='Nome')
 		self.tree.heading('#3', text='Quantidade de pessoas')
-		self.tree.heading('#4', text='Tempo')
+		self.tree.heading('#4', text='Tempo agendado')
 		self.tree.grid(column=0, row=0, sticky='nsew', in_=menu)
+		menu.grid_columnconfigure(0, weight=1)
+		menu.grid_rowconfigure(0, weight=1)
 		
-		
-		#menu.grid_columnconfigure(0, weight=1)
-		#menu.grid_rowconfigure(0, weight=1)
-		if data in self.dados.horarios.keys():
-			for i in range(len(self.dados.horarios[data])):
-				for hora in self.dados.horarios[data][i]:
-						for z in range(len(self.dados.horarios[data][i][hora])):
-							self.tree.insert('', 'end', text=hora, values=(self.dados.horarios[data][i][hora][z]["setor"], self.dados.horarios[data][i][hora][z]["nome"], self.dados.horarios[data][i][hora][z]["numero de pessoas"], self.dados.horarios[data][i][hora][z]["tempo"]))
+		if 	datetime.datetime(ano, mes, dia).strftime("%A") == "Thursday":
+			self.tree.insert('', 'end', text = "   Não é possível", values =("fazer agendamentos", "de Quinta-feira"),tag='monospace')
+			self.tree.tag_configure('monospace', font='courier')
+
+		else:
+			if data in self.dados.horarios.keys():
+				for i in range(len(self.dados.horarios[data])):
+					for hora in self.dados.horarios[data][i]:
+							for z in range(len(self.dados.horarios[data][i][hora])):
+								self.tree.insert('', 'end', text=hora, values=(self.dados.horarios[data][i][hora][z]["setor"], self.dados.horarios[data][i][hora][z]["nome"], self.dados.horarios[data][i][hora][z]["numero de pessoas"], self.dados.horarios[data][i][hora][z]["tempo"]))
 
 	def novadata(self,item,data, dados, menu):
 		self.tree.destroy()
