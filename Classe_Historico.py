42cf1d427285705484ad74be6147a12960d0b754
annabcm <annabcm@al.insper.edu.br>
2017-06-01 18:51:58 -0300

    firebase

--- Classe_Historico.py
+++ Classe_Historico.py
@@ -7,6 +7,11 @@ class Historico:
 	def __init__(self,frame, dados, usuario):
 		self.usuario = usuario
 		self.dados = dados
+		self.horas={'8:00': 800,'8:30': 850,'9:00':900,'9:30':950,'10:00':1000,'10:30':1050,'11:00':1100,'11:30':1150,'12:00':1200,'12:30':1250,'13:00':1300,'13:30':1350,'14:00':1400,'14:30':1450,'15:00':1500,'15:30':1550,'16:00':1600,'16:30':1650,'17:00': 1700,'17:30': 1750,'18:00':1800,'18:30':1850}
+	
+		
+		
+		
 		titulo=Label(frame, text="Histórico", height=1, font=("Helvetica", 30, 'bold'))
 		titulo.grid(rowspan=1, columnspan = 6, pady = 15)       #título
 
@@ -21,15 +26,24 @@ class Historico:
 		frame.grid_rowconfigure(0, weight=1)
 		
 		self.datas = {}
-		for data in self.dados.historia.keys():
-			for hora in self.dados.historia[data].keys():
-					for setor in self.dados.historia[data][hora].keys():
-						self.tree.insert('','end', values = (data, setor, self.dados.historia[data][hora][setor]["numero de pessoas"], self.dados.historia[data][hora][setor]["tempo"],self.dados.historia[data][hora][setor]["status"]))
-						dia = int(data[0:2])
-						mes = int(data[3:5])
-						ano = int(data[6:])
-						data_final = dia+(mes*100)+(ano*1000000)
-						self.datas[data] = data_final
+		
+		diadb = self.dados.dias2()
+		
+		for dia in diadb:
+			mesdb = self.dados.meses2(dia)
+			for mes in mesdb:
+				anodb = self.dados.anos2(dia,mes)
+				for ano in anodb:
+					data = "{}/{}/{}".format(dia,mes,ano)
+					for hora in self.dados.horas2(data):
+						for setor in self.dados.setor2(data,hora):
+							val = self.dados.valores2(data,hora,setor)
+							self.tree.insert('','end', values = (data, setor, val[0], val[1],val[2]))
+							dia = int(data[0:2])
+							mes = int(data[3:5])
+							ano = int(data[6:])
+							data_final = dia+(mes*100)+(ano*1000000)
+							self.datas[data] = data_final
 						
 		self.tree.column('Data', anchor = CENTER)
 		self.tree.column('Setor', anchor = CENTER)
