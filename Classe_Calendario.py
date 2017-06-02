0039fbbfc260f78efc5fdad2371aa990254d8bb6
DennisZF <denniszf@al.insper.edu.br>
2017-06-02 07:48:28 -0300

    código comentado

--- Classe_Calendario.py
+++ Classe_Calendario.py
@@ -11,16 +11,18 @@ class Calendario:
 		self.menu = menu
 		
 		from Classe_Calendar2 import Calendar2
-	
+		#----título da página
 		titulo = Label(frame, text="Calendário", height=1 ,font=("Helvetica", 30, 'bold'))
 		titulo.pack(side= TOP, pady=25)
 		
+		#-------instrução
 		data = Label(frame, text="Escolha a data que deseja visualizar", height=1 ,font=("Helvetica", 13, 'bold'))
 		data.pack(side = TOP)
 		
 		self.escolha = Label(frame, text=["Selecione uma data"], font=("Helvetica", 11, 'bold'))
 		self.escolha.pack(side = TOP, pady= 15)
 		
+		#------- calendário e sua geometrização
 		calendario = Calendar2(frame)
 		calendario.pack(side = TOP)
 		
@@ -28,7 +30,7 @@ class Calendario:
 		calendario.quando_selecionada(self.atualiza)
 		
 		
-	
+	#------função de atualização 
 	def atualiza(self,x):
 		self.escolha['text'] = x;
 		from Classe_Horarios import Horarios
