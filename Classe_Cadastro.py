f6cb25aa5733e96aaea2fd1b8692270a99210b8c
annabcm <annabcm@al.insper.edu.br>
2017-06-01 23:28:08 -0300

    enter

--- Classe_Cadastro.py
+++ Classe_Cadastro.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import tkinter.ttk as ttk
+
+
 class Cadastro:
 		def __init__(self, dados):
 			self.dados = dados
@@ -6,6 +9,7 @@ class Cadastro:
 			self.pagina_nova= Tk()								#Abre uma nova janela
 			self.pagina_nova.title("Cadastro - FabLab Insper")
 			self.pagina_nova.wm_iconbitmap("icon_bitmap.ico")
+			self.pagina_nova.bind("<Return>", self.envia)
 
 			titulo_cadastro = Label(self.pagina_nova, text="Ficha de cadastro", height=1 ,font=("Helvetica", 25, 'bold'))			#Título da página
 			titulo_cadastro.grid(row=0, columnspan=2)
@@ -21,9 +25,10 @@ class Cadastro:
 			#Curso
 			pede_curso = Label(self.pagina_nova, text="Curso:", height=1 ,font=("Helvetica", 10))
 			pede_curso.grid(row=5, column = 0)
-			self.curso = Entry(self.pagina_nova, width= 25)
-			self.curso.grid(row=5, column = 1) 
-			
+			self.curso=ttk.Combobox(self.pagina_nova,width= 22)
+			self.curso.grid(row=5, column=1)  #combobox de seleção
+			self.curso['values']=('Administração','Economia','Engenharia de Computação','Engenharia Meacânica','Engenharia Mecatrônica', 'Outro')
+			self.curso.current(0)
 			
 			#Semestre
 			pede_semestre = Label(self.pagina_nova, text="Semestre:", height=1 ,font=("Helvetica", 10))
@@ -66,23 +71,28 @@ class Cadastro:
 			enviar.grid(rowspan = 19, column = 1, pady=5)
 			
 		#------- Função -------
-		def envia(self):
+		def envia(self, event = None):
 			tudo = self.preenchido()
 			if tudo == True:
-				if self.senha.get() == self.repete.get():
-					add = self.dados.usuarioexistente(self.usuario.get(), self.senha.get(),self.nome.get(),self.curso.get(), self.semestre.get(), self.matricula.get(), self.email.get())
-					if add == True:
-						self.pagina_nova.destroy()
+				if self.curso.get() in self.curso['values']:
+					if self.senha.get() == self.repete.get():
+						add = self.dados.usuarioexistente(self.usuario.get(), self.senha.get(),self.nome.get(),self.curso.get(), self.semestre.get(), self.matricula.get(), self.email.get())
+						if add == True:
+							self.pagina_nova.destroy()
+						else:
+							if hasattr(self,'notifica'):
+								self.notifica.destroy()
+							self.notifica= Label(self.pagina_nova, text="Usuário já existente",fg= 'red', justify=CENTER,font=("Helvetica", 10, 'bold'))					#"Texto que se refere ao link
+							self.notifica.grid(row = 2, columnspan = 2)
 					else:
 						if hasattr(self,'notifica'):
-							self.notifica.destroy()
-						self.notifica= Label(self.pagina_nova, text="Usuário já existente",fg= 'red', justify=CENTER,font=("Helvetica", 10, 'bold'))					#"Texto que se refere ao link
+								self.notifica.destroy()
+						self.notifica= Label(self.pagina_nova, text="Senhas não compatíveis",fg= 'red', justify=CENTER,font=("Helvetica", 10, 'bold'))					#"Texto que se refere ao link
 						self.notifica.grid(row = 2, columnspan = 2)
 				else:
-					if hasattr(self,'notifica'):
-							self.notifica.destroy()
-					self.notifica= Label(self.pagina_nova, text="Senhas não compatíveis",fg= 'red', justify=CENTER,font=("Helvetica", 10, 'bold'))					#"Texto que se refere ao link
+					self.notifica= Label(self.pagina_nova, text="Preencha todos os campos adequadamente",fg= 'red', justify=CENTER,font=("Helvetica", 10, 'bold'))					#"Texto que se refere ao link
 					self.notifica.grid(row = 2, columnspan = 2)
+				
 			else:
 				for info in tudo:
 					info.configure(bg = 'Khaki1')
@@ -92,7 +102,7 @@ class Cadastro:
 			
 		def preenchido(self):
 			vazios=[]
-			for x in [self.usuario, self.senha, self.nome,self.curso, self.semestre, self.matricula, self.email, self.repete, self.curso]:
+			for x in [self.usuario, self.senha, self.nome, self.semestre, self.matricula, self.email, self.repete, self.curso]:
 				a = x.get()
 				if a == "":
 					vazios.append(x)
